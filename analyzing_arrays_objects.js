1ab62c00b6603a085b020e84dfde2058082501c6
jaywomack <womackjw@hotmail.com>
2021-03-08 14:41:39 -0800

    Startign problem solving approach

--- analyzing_arrays_objects.js
+++ analyzing_arrays_objects.js
@@ -1,1 +1,26 @@
 // The big O of objects
+/**
+ * BIG O of objects
+ * insertion- is constant O(1)
+ * Removal- is constant O(1)
+ * Searching- is in proportion to the number of items o(n)
+ * Access- is constant O(1)
+
+*/
+
+/***
+ * Arrays
+ *
+ * When are arrays slow?
+ * Arrays are ordered lists
+ * Arrays should be used when you need order
+ * When using an index O(1)
+ *
+ * when inserting into an array it depends where the value is inserted
+ * pushing onto the end of thar array O(n)
+ * when inserting at the beginning of an array O(n)
+ *
+ * Adding and removing from the beginning of an array is not efficient as if you were to add add and remove from the end of an array
+ *
+ * So push and pop are more efficient than shift and unshift
+ */
